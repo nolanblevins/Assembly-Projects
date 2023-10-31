c0cca632ff1434995d10c8827b099122d9bb7127
Nolan Blevins <npblevins@gmail.com>
2023-10-31 13:28:36 -0400

    Add files via upload

--- c2.asm
+++ c2.asm
@@ -1,126 +1,109 @@
-.data 
-	aInt:.asciiz "Enter value for a: "
-	bInt:.asciiz "Enter value for b: "
-	cInt:.asciiz "Enter value for c: "
-	overflow:.asciiz "Overflow: "
-	noOverflow:.asciiz "No overflow: "
-	divError:.asciiz "Division by zero "
-	
-	
+.data
+weightPrompt: .asciiz "Enter your weight (in pounds): "
+heightPrompt: .asciiz "Enter your height (in feet): "
+bmi: .asciiz "Your BMI is: "
+underweight: .asciiz "You are underweight."
+normal: .asciiz "You are normal weight."
+overweight: .asciiz "You are overweight."
+newline: .asciiz "\n"
+ow:.float 25.0
+uw:.float 18.5
+hC:.float 12.0   #weight conversion
+bM:.float 703.0 # bmi multiplier
+
 .text
 main:
-#-----------------------------------------------
-# A
-    li $v0, 4 # system call for printing string
-    la $a0, aInt # load the address of the prompt for a
+
+#-----------------------------------------------------------
+# WEIGHT
+li $v0, 4 				# expect string
+la $a0, weightPrompt 	#enter weight message
+syscall
+
+li $v0, 6 				# expect a float
+syscall
+mov.s $f1, $f0 			# move float of weight to f1
+
+#-----------------------------------------------------------
+# HEIGHT
+li $v0, 4 				#expect string
+la $a0, heightPrompt 	# enter height message
+syscall
+
+li $v0, 6 				# expect a float
+syscall
+mov.s $f2, $f0			# Move the height to the $f2
+
+# convert height to inches
+l.s $f3, hC
+mul.s $f2, $f2, $f3
+
+#-----------------------------------------------------------
+# BMI CALC
+mul.s $f2, $f2, $f2		# square height
+div.s $f4, $f1, $f2		# weight divided by height
+l.s $f5, bM
+mul.s $f6, $f4, $f5		# times 703
+
+#-----------------------------------------------------------
+# PRINT BMI
+li $v0, 4 # expect string
+la $a0, bmi
+syscall
+    li $v0, 2      			# expect float
+    mov.s $f12, $f6 		# move bmi to f12
     syscall
-
-    # Read the input value for a
-    li $v0, 5 # system call for reading an integer
+    li $v0, 4			
+    la $a0, newline		# newline
     syscall
-    move $s0, $v0
+ #-----------------------------------------------------------
+ # FINDING BMI CLASS
+l.s $f7, uw			# load underweight value
+l.s $f8, ow			# load overweight value
+ 
+c.lt.s $f6, $f7			# bmi is less than 18.5
+bc1t underweightBMI 	# go to underweight print
+c.lt.s $f7, $f6			# bmi more than 18.5
+c.lt.s $f6, $f8			# bmi less than 25
+bc1t normalBMI		# go to normal weight print
+c.lt.s $f8, $f6			# bmi more than 25
+bc1t overweightBMI 	# go to overweight print
 
-#-----------------------------------------------
-# B
-    li $v0, 4 # system call for printing string
-    la $a0, bInt # load the address of the prompt for b
+#-----------------------------------------------------------
+# UNDERWEIGHT PRINT
+
+underweightBMI:
+    li $v0, 4
+    la $a0, underweight 
     syscall
-
-    # Read the input value for b
-    li $v0, 5 # system call for reading an integer
+    
+    li $v0, 4
+    la $a0, newline
     syscall
-    move $s1, $v0
+    j main
+#-----------------------------------------------------------
+# NORMAL PRINT
 
-#-----------------------------------------------
-# C
-    li $v0, 4 # system call for printing string
-    la $a0, cInt # load the address of the prompt for c
+normalBMI:
+    li $v0, 4
+    la $a0, normal
     syscall
-
-    # Read the input value for c
-    li $v0, 5 # system call for reading an integer
+    
+    li $v0, 4
+    la $a0, newline
     syscall
-    move $s2, $v0
-#-----------------------------------------------
-# a + b ^ c
-# -----------  + (3 * b)
-# c - a ^ 2
-	
-#-----------------------------------------------
-numerator:
-	move $t0, $s1    # move b to $t0
-	move $t2, $s1    # move b to $t2
-	move $t1, $s2    # move c to $t1
-	j loop
-loop:
-	blt  $t1, 2, loopDone  # exit loop when c = 0
-	mul $t0, $t2, $t0           # result of prev b * curr b in b
-	mflo $t3                   # Move the lower 32 bits of the result to $t1
-	
-	slt $s6, $t3, $zero         # Check if the lower 32 bits of the result are negative
-	xor $s5, $t0, $t3           # Check if the sign of the lower 32 bits is different from the sign of the upper 32 bits
-	and $s6, $s6, $s5           # Check if both conditions are true
-	bne $s6, $zero, overflowPrint    # Branch to the overflow label if overflow has occurred
+    j main
+#-----------------------------------------------------------
+# OVERWEIGHT PRINT
 
-	subi $t1, $t1, 1           # decrement c
-	j loop                # repeat loop
-	
-loopDone:
-	add $t4, $s0, $t0  # add a and b^c, store result in $t4
-	j denominator
-# numerator stored in $t4
-#-----------------------------------------------
+overweightBMI:
+    li $v0, 4
+    la $a0, overweight
+    syscall
+    
+    li $v0, 4
+    la $a0, newline
+    syscall
+    j main
 
-denominator:	# c - a * a
-	move $t6, $s0 #t6 = a
-	move $t7, $s2 #t7 = c
-	mul $t6, $t6, $t6 # t6 = a * a
-	sub $t7, $t7, $t6 # t7 = c - a * a
-	j division
-# denominator stored in $t7
-	
-#-----------------------------------------------
-division:
-	beqz $t7, divByZero	# branch if div by zero true
-	div $t5, $t4, $t7 # else do division
-	j additive
 
-#-----------------------------------------------
-additive:	# 3 * b
-	li $t8, 3
-	move $t9, $s1 # stiore b
-	mul $t9, $t9, $t8 # b* 3
-	add $t9, $t9, $t5 # inequlaity + (3*b)
-	j print
-#-----------------------------------------------
-print:
-	li $v0, 4 # system call for printing string
-   	la $a0, noOverflow 
-   	syscall
-	li $v0, 1        # system call for printing an integer
-	move $a0, $t9     
-	syscall          
-	li $v0, 10       # system call for exiting the program
-	syscall       
-#-----------------------------------------------
-overflowPrint:
-	 li $v0, 4 # system call for printing string
-   	 la $a0, overflow 
-   	 syscall
-   	 li $v0, 10       # system call for exiting the program
-	syscall  
-#-----------------------------------------------
-divByZero:
-	li $v0, 4 # system call for printing string
-	la $a0, divError
-	syscall
-	li $v0, 10       # system call for exiting the program
-	syscall  
-	
-
-	
-
-	
-	
-	
-	
--- c3.asm
+++ c3.asm
@@ -1,137 +1,126 @@
 .data
+promptNumHw: .asciiz "Enter the number of homeworks: "
+promptAvgHwTime: .asciiz "Enter the average time per homework: "
+promptNumEx: .asciiz "Enter the number of exercises: "
+promptAvgExTime: .asciiz "Enter the average time per exercise: "
+totalWork: .asciiz "Total work: "
 
-firstInCount: .asciiz "Enter the first Instruction count value:  "
-firstCPI: .asciiz "Enter the first CPI value:  "
-firstClkRate:.asciiz "Enter the first Clock Rate:  "
-secInCount: .asciiz "Enter the second Instruction count value:  "
-secCPI: .asciiz "Enter the second CPI value:  "
-secClkRate:.asciiz "Enter the second Clock Rate:  "
-firstCPU:.asciiz "A is "
-firstCPU2:.asciiz " times as fast as B"
-secCPU:.asciiz "B is "
-secCPU2:.asciiz " times as fast as A"
-
-
-
-#	(CPU TIME = INSTRUCTION_COUNT * CPI) / CLOCK_RATE
 .text
-
 main:
-#-----------------------------------------------
-# First Instruction Count
-	li $v0, 4
-	la $a0, firstInCount
-	syscall
-	
-	li $v0, 6
-	syscall
-	mov.s $f2, $f0
-#------------------------------------------------
-# First CPI Value
-	li $v0, 4
-	la $a0, firstCPI
-	syscall
-	
-	li $v0, 6
-	syscall
-	mov.s $f3, $f0
-#------------------------------------------------
-# First Clock Rate
-	li $v0, 4
-	la $a0, firstClkRate
-	syscall
-	
-	li $v0, 6
-	syscall
-	mov.s $f4, $f0
-#------------------------------------------------
-# Second Instruction Count
-	li $v0, 4
-	la $a0, secInCount
-	syscall
-	
-	li $v0, 6
-	syscall
-	mov.s $f5, $f0
-#------------------------------------------------
-# Second CPI Value
-	li $v0, 4
-	la $a0, secCPI
-	syscall
-	
-	li $v0, 6
-	syscall
-	mov.s $f6, $f0
-#------------------------------------------------
-# Second Clock Rate
-	li $v0, 4
-	la $a0, secClkRate
-	syscall
-	
-	li $v0, 6
-	syscall
-	mov.s $f7, $f0
-#------------------------------------------------
-# f2 = first instruction count
-# f3 = first CPI Value
-# f4 = first clock rate
-# f5 = second instruction cound
-# f6 = second cpi value
-# f7 = second clock rate
-#	(CPU TIME = INSTRUCTION_COUNT * CPI) / CLOCK_RATE
-#--------------------
-# first CPU time = f8
-mul.s $f8, $f2, $f3
-div.s $f8, $f8, $f4
-#--------------------
-# second CPU time = f9
-mul.s $f9, $f5, $f6
-div.s $f9, $f9, $f7
-#--------------------
-c.lt.s $f8, $f9
-bc1t CPU1
-c.lt.s $f9, $f8
-bc1t CPU2
+#-----------------------------------------------------------
+# NUM HW
+li $v0, 4						# load v0 to expect string
+la $a0, promptNumHw			# load prompt into a0 for num HW
+syscall				
+li $v0, 5						# load v0 to expect a int
+syscall						# read int and place in $t0
+move $s0, $v0
 
-#--------------------
-CPU2:
-	div.s $f10, $f8, $f9
-	
-	li $v0, 4
-	la $a0, secCPU
-	syscall
-	
-	li $v0,2
-	mov.s $f12, $f10
-	syscall
-	
-	li $v0, 4
-	la $a0, secCPU2
-	syscall
-	
-	li $v0, 10
-	li $a0, 0	
-	syscall
+#-----------------------------------------------------------
+# HRS PER HW
+li $v0, 4						# load v0 to expect string
+la $a0, promptAvgHwTime		# load prompt into a0 for average hw time
+syscall				
+li $v0, 5						# load v0 to expect a int
+syscall						# read int and place in $t0
+move $s1, $v0
 
-	
-	
-	
-CPU1:
-	div.s $f11, $f9, $f8
-	
-	li $v0, 4
-	la $a0, firstCPU
-	syscall
-	
-	li $v0,2
-	mov.s $f12, $f11
-	syscall
-	
-	li $v0, 4
-	la $a0, firstCPU2
-	syscall
-	
-	li $v0, 10
-	li $a0, 0	
-	syscall
-	
+#-----------------------------------------------------------
+# NUM EXERCISES
+li $v0, 4						# load v0 to expect string
+la $a0, promptNumEx			# load prompt into a0 for num exercises
+syscall				
+li $v0, 5						# load v0 to expect a int
+syscall						# read int and place in $t0
+move $s2, $v0
+
+#-----------------------------------------------------------
+# HRS PER EXERCISE
+li $v0, 4						# load v0 to expect string
+la $a0, promptAvgExTime		# load prompt into a0 for avg ex time
+syscall				
+li $v0, 5						# load v0 to expect a int
+syscall						# read int and place in $t0
+move $s3, $v0
+
+#-----------------------------------------------------------
+# MOVE INPUTS FOR CALC
+move $a0, $s0
+move $a1, $s1
+move $a2, $s2
+move $a3, $s3
+
+#-----------------------------------------------------------
+# call total func
+jal total
+# put ret in s6
+move $s6, $v0
+#-----------------------------------------------------------
+# PRINT
+
+# print total
+li $v0, 4
+la $a0, totalWork
+syscall
+li $v0, 1
+move $a0, $s6
+syscall
+
+#exit
+li $v0, 10
+syscall
+
+#-----------------------------------------------------------
+# HW FUNC
+hwFunction:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    # Multiply num hws by avg hours
+    mul  $t0, $a0, $a1
+    add $v0, $zero, $t0 # place value in return
+    # Restore the return address and return
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
+    
+#-----------------------------------------------------------
+# EXERCISE FUNC
+exFunction:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    # Multiply num exercises by avg hours
+    mul $t1, $a0, $a1
+    add $v0, $zero, $t1 # place value in return
+    # Restore the return address and return
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
+    
+#-----------------------------------------------------------
+# TOTAL FUNC
+total:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+    # Call hw func with num hws and avg hours
+    jal hwFunction
+    move $t2, $v0
+    
+    # Call exercise func with num exercises and avg hours
+    move $a0, $a2 # move to a0 for func call
+    move $a1, $a3 # move to a1 for func call
+    jal exFunction
+    move $t3, $v0 # move return to t3
+    
+    # Add the results of hw func and exercise func
+    add $v0, $t3, $t2
+    
+    # Restore the return address and return
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
+    
+  
+
+
+
+
